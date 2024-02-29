95d0ffe915079c333d8a1a2ccdd61835c19cb805
maddrills <mathewfvv@gmail.com>
2024-02-29 12:27:17 +0530

    time check

--- Big O/hello/hello.js
+++ Big O/hello/hello.js
@@ -7,11 +7,15 @@ const nemo = ['nemo'];
 
 const name = function (array){
 
+    let time0 = performance.now();
     array.forEach((ele,index,fullarray) => {
         if(ele == 'nemo'){
             console.log('found nemo');
         }
     })
+    let time1 = performance.now();
+
+    console.log("time diff is: ",time1 - time0 );
 }
 
 name(nemo);
