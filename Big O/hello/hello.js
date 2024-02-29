a5eb8de2888a74ed70dca639f51ce7594d476af5
maddrills <mathewfvv@gmail.com>
2024-02-29 12:02:47 +0530

    loop d loop

--- Big O/hello/hello.js
+++ Big O/hello/hello.js
@@ -2,3 +2,16 @@ console.log("Hello World");
 console.log("Hello World");
 //git push --set-upstream origin main
 //third
+
+const nemo = ['nemo'];
+
+const name = function (array){
+
+    array.forEach((ele,index,fullarray) => {
+        if(ele == 'nemo'){
+            console.log('found nemo');
+        }
+    })
+}
+
+name(nemo);
